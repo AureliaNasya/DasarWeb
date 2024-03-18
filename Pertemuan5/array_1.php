31f2451122eed7b046c0ab34dbefd752f31d8e18
AureliaNasya <aurelia.nasya93@gmail.com>
2024-03-18 10:15:22 +0700

    Pertemuan 5_Praktikum 1

---
+++ Pertemuan5/array_1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <body>
+            <h2>Array Terindeks</h2>
+            <?php
+            $Listdosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+            echo $Listdosen[2] . "<br>";
+            echo $Listdosen[0] . "<br>";
+            echo $Listdosen[1] . "<br>";
+            ?>
+            <br>
+            <h2>Array Terindeks (Perulangan)</h2>
+            <?php
+            foreach($Listdosen as $dosen) {
+                echo $dosen . "<br>";
+            }
+            ?>
+        </body>
+    </head>
+</html>
