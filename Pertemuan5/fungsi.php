77496aec2c570a529c854451bd5e6b2a9ef59bf4
AureliaNasya <aurelia.nasya93@gmail.com>
2024-03-18 11:07:42 +0700

    Pertemuan 5_Praktikum 4

--- Pertemuan5/fungsi.php
+++ Pertemuan5/fungsi.php
@@ -1,12 +1,7 @@
 <?php
-function perkenalan($nama, $salam = "Shalom") {
-    echo $salam. ", ";
-    echo "Perkenalkan nama saya ".$nama. "<br>";
-    echo "Senang berkenalan dengan Anda <br>";
+function hitungUmur($thn_lahir, $thn_sekarang) {
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
 }
-perkenalan("Hamdana", "Hallo");
-echo "<hr>";
-$saya = "Elok";
-$ucapanSalam = "Selamat pagi";
-perkenalan($saya);
+echo "Umur saya adalah ". hitungUmur(2003, 2024) ." tahun";
 ?>
