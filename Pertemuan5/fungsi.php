e31c2da19463b764345be7c066266281035207d1
AureliaNasya <aurelia.nasya93@gmail.com>
2024-03-18 10:52:23 +0700

    Pertemuan 5_Praktikum 4

---
+++ Pertemuan5/fungsi.php
@@ -0,0 +1,9 @@
+<?php
+function perkenalan() {
+    echo "Shalom, ";
+    echo "Perkenalkan nama saya Nasya <br>";
+    echo "Senang berkenalan dengan Anda <br>";
+}
+perkenalan();
+perkenalan();
+?>
