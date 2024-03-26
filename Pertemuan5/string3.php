e4d622d518331e5b125a8d721c4824f3db84bb00
AureliaNasya <aurelia.nasya93@gmail.com>
2024-03-26 13:00:25 +0700

    Pertemuan 5_Praktikum 6

--- Pertemuan5/string3.php
+++ Pertemuan5/string3.php
@@ -1,4 +1,7 @@
 <?php
 $pesan = "Saya arek ngalam";
-echo strrev($pesan). "<br>";
+$pesanPerKata = explode(" ", $pesan);
+$pesanPerKata = array_map(fn($pesan) => strrev($pesan), $pesanPerKata);
+$pesan = implode(" ", $pesanPerKata);
+echo ($pesan). "<br>";
 ?>
