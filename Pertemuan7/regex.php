4a311c7fe606a3cd52913e8ecaafac34a1c1a72c
AureliaNasya <aurelia.nasya93@gmail.com>
2024-04-01 10:34:27 +0700

    Pertemuan 7 _ Praktikum 5

--- Pertemuan7/regex.php
+++ Pertemuan7/regex.php
@@ -1,10 +1,10 @@
 <?php
-$pattern = '/[a-z]/'; //cocokan huruf kecil
-$text = 'This is a Sample Text';
+$pattern = '/[0-9]+/'; //cocokan satu atau lebih digit
+$text = 'There are 123 apples';
 echo $text. "<br>";
-if (preg_match($pattern, $text)) {
-    echo "Huruf kecil ditemukan!";
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokan : " .$matches[0];
 } else {
-    echo "Tidak ada huruf kecil!";
+    echo "Tidak ada yang cocok!";
 }
 ?>
