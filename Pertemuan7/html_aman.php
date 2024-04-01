ca6e423ae1676816a877ad832bc4fcfae9ec4616
AureliaNasya <aurelia.nasya93@gmail.com>
2024-04-01 10:24:26 +0700

    Pertemuan 7 _ Praktikum 4

--- Pertemuan7/html_aman.php
+++ Pertemuan7/html_aman.php
@@ -1,6 +1,14 @@
 <?php
 $input = $_POST['input'];
 $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+
+$email = $_POST['email'];
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $subject = 'Email Validation';
+    $pesan = 'Email Anda Valid : ' .$email. "<br>";
+} else {
+    echo "Email Anda Tidak Valid <br>";
+}
 ?>
 
 <!DOCTYPE html>
@@ -8,7 +16,8 @@ $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
     <head></head>
     <body>
         <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        <input type="text" name="input">
+        <label for="email">Email : </label>
+        <input type="text" name="email">
         <button type="submit">Submit</button>
         </form>
     </body>
