3efa32036d48ec3603beb7327624c42e66e50502
AureliaNasya <aurelia.nasya93@gmail.com>
2024-04-20 22:40:31 +0700

    Pertemuan 8 _ Praktikum 1

--- Pertemuan8/upload.php
+++ Pertemuan8/upload.php
@@ -2,11 +2,18 @@
 if (isset($_POST["submit"])) {
     $targetDirectory = "uploads/"; //direktori tujuan untuk menyimpan file
     $targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
+    $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+    $maxFileSize = 5 * 1024 * 1024;
 
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
-        echo "File berhasil diunggah";
+    if (in_array($fileType, $allowedExtensions) && $_FILES["fileToUpload"]["size"] <= $maxFileSize) {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
+            echo "File berhasil diunggah";
+        } else {
+            echo "Gagal mengunggah file";
+        }
     } else {
-        echo "Gagal mengunggah file";
+        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan";
     }
 }
 ?>
