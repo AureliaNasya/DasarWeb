f326a88edc800f55b12a4db0892e1b6c60e3e9a0
AureliaNasya <aurelia.nasya93@gmail.com>
2024-04-20 21:38:57 +0700

    Pertemuan 7 _ Praktikum 6

--- Pertemuan7/form_lanjut.php
+++ Pertemuan7/form_lanjut.php
@@ -30,23 +30,3 @@
         </from>
     </body>
 </html>
-
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $selectedBuah = $_POST['buah'];
-    if (isset($_POST['warna'])) {
-        $selectedWarna = $_POST['warna'];
-    } else {
-        $selectedWarna = [];
-    }
-    $selectedJenisKelamin = $_POST['jenis_kelamin'];
-    echo "Anda Memilih Buah : " .$selectedBuah. "<br>";
-    if (!empty($selectedWarna)) {
-        echo "Warna Favorit Anda : " .implode(", ", $selectedWarna). "<br>";
-    } else {
-        echo "Anda tidak memilih warna favorit <br>";
-    }
-
-    echo "Jenis Kelamin Anda : " .$selectedJenisKelamin;
-}
-?>
