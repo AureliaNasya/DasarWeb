cf975e6b53958682218bd9e58954f030542d08e7
AureliaNasya <aurelia.nasya93@gmail.com>
2024-04-20 23:09:54 +0700

    Pertemuan 8 _ Praktikum 1

--- Pertemuan8/upload.php
+++ Pertemuan8/upload.php
@@ -1,19 +1,19 @@
 <?php
 if (isset($_POST["submit"])) {
-    $targetDirectory = "uploads/"; //direktori tujuan untuk menyimpan file
-    $targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
-    $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+    $targetDirectory = "documents/"; //direktori tujuan untuk menyimpan file
+    $targetFile = $targetDirectory . basename($_FILES["documentToUpload"]["name"]);
+    $documentFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
     $allowedExtensions = array("txt", "pdf", "doc", "docx");
     $maxFileSize = 10 * 1024 * 1024;
 
-    if (in_array($fileType, $allowedExtensions) && $_FILES["fileToUpload"]["size"] <= $maxFileSize) {
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
-            echo "File berhasil diunggah";
+    if (in_array($documentFileType, $allowedExtensions) && $_FILES["documentToUpload"]["size"] <= $maxFileSize) {
+        if (move_uploaded_file($_FILES["documentToUpload"]["tmp_name"], $targetFile)) {
+            echo "Dokumen berhasil diunggah";
         } else {
-            echo "Gagal mengunggah file";
+            echo "Gagal mengunggah dokumen";
         }
     } else {
-        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan";
+        echo "Jenis dokumen tidak valid atau melebihi ukuran maksimum yang diizinkan";
     }
 }
 ?>
